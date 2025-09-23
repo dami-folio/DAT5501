a2bd9ec8791652cad1cf18d8fd96b3403e524df8
Dami <dami.akinsemoyin@gmail.com>
2025-09-23 12:30:36 +0100

    Add files via upload
    Created 'bakery sim project.py' and outlined project goals within the file.

---
+++ bakery sim project.py
@@ -0,0 +1,16 @@
+# The goal of this project is to exercise my understanding of Python fundamentals and classes by making a bakery simulator. I've worked on this exact project before,
+# but with limited success due to my lack of understanding and experience. As such, I am returning to try and complete a renewed version of the original code. 
+
+# The bakery should have classes for menus, in which menu items are the class objects, and it should be possible to do the following:
+#   - assign item types (e.g. baked good, beverage, confection, etc. )
+#   - add menu items
+#   - remove menu items
+#   - assign prices to menu items within a price range (so that users can't sell drinks for £100)
+#   - display menu items in a list (either all items at once or types of items at once)
+#   - check menu item attributes (such as item name, price, type and whether or not it's currently discounted)
+
+# Additionally, I'd like to try and create a basic customer algorithm so that selling items is actually possible. I would like for customers to have randomised budgets,
+# and for them to be able to buy any number of bakery items as long as the total price falls within their budget. This is a secondary goal for this project, as the main
+# goal is to practice object-oriented programming concepts and programming fundamentals.
+
+print("This is a test.")
