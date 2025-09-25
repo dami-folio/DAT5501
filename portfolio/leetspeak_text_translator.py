08a179f889e96aa36d5d6a55bc6c86f69b8cbd3e
dami-folio <dami.akinsemoyin@gmail.com>
2025-09-25 15:06:11 +0100

    added commented documentation to the leetspeak translator file.

--- portfolio/leetspeak_text_translator.py
+++ portfolio/leetspeak_text_translator.py
@@ -2,7 +2,7 @@
 
 # rules: a = 4, e = 3, o = 0, s = 5, i = 1
 
-translation_dict = {
+translation_dict = { # translation dictionary designed according to the translation rules.
     "a": 4,
     "e": 3,
     "i": 1,
@@ -11,12 +11,12 @@ translation_dict = {
 }
 
 def translate(phrase):
-   translation = ""
+   translation = "" # empty string to contain the translated phrase later on. 
    for letter in phrase:
-       if letter.lower() in translation_dict:
-            translation += str(translation_dict[letter.lower()])
+       if letter.lower() in translation_dict: # checks if the lowercase character can be found in the translation dictionary.
+            translation += str(translation_dict[letter.lower()]) # if the character can be found in the dictionary, find the key under the same name and swap 'letter' with the key's value.
        else:
-           translation += letter
-   return translation.upper()
+           translation += letter # otherwise, add the letter onto the translation string as it is
+   return translation.upper() # returns the translation in uppercase to complete the function.
 
 print(translate(input("Tell me somethin' and I'll say it back my way! ")))
