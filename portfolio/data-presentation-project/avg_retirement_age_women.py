089fc8695b580b94a8274f19a9f5ac2a9213b35d
dami-folio <dami.akinsemoyin@gmail.com>
2025-10-11 12:07:21 +0100

    minor contribution to analysis program; added a line of code to check that the year column has been appropriately changed to the datetime datatype.

--- portfolio/data-presentation-project/avg_retirement_age_women.py
+++ portfolio/data-presentation-project/avg_retirement_age_women.py
@@ -1,7 +1,10 @@
-# GOAL: import retirement age csv.
-
 import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import date, timedelta
+import numpy as np
 
-retirement_age_df = pd.read_csv('retirement_age_men_and_women.csv')
-print(retirement_age_df.head)
+# the below code formats the 'year' column to be read as a year rather than a number by the analysis modules.
+retirement_age_df = pd.read_csv('retirement_age_men_and_women.csv', parse_dates = ['Year'])
+
+print(type(retirement_age_df['Year'][0])) # quickly checking that parsing the dates changed the data type properly.
 
