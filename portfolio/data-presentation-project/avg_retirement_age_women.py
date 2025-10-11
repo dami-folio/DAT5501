14dae7439d8b1cab3ff8ff55388a5f4d5fcc1e9d
dami-folio <dami.akinsemoyin@gmail.com>
2025-10-11 13:00:26 +0100

    successfully recalled how to apply masks to dataframes; changed column names of the retirement age csv

--- portfolio/data-presentation-project/avg_retirement_age_women.py
+++ portfolio/data-presentation-project/avg_retirement_age_women.py
@@ -6,5 +6,9 @@ import numpy as np
 # the below code formats the 'year' column to be read as a year rather than a number by the analysis modules.
 retirement_age_df = pd.read_csv('retirement_age_men_and_women.csv', parse_dates = ['Year'])
 
-print(type(retirement_age_df['Year'][0])) # quickly checking that parsing the dates changed the data type properly.
+# print(type(retirement_age_df['Year'][0])) # quickly checking that parsing the dates changed the data type properly.
 
+# create a mask for the df and apply it to the main csv.
+oecd_mask = retirement_age_df['Entity'] == 'OECD'
+oecd_df = retirement_age_df[oecd_mask]
+print(oecd_df)
