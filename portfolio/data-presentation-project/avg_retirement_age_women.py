7992d8b37a1d9f2ee9eaa4c80dfb9fd1cf31fe1e
dami-folio <dami.akinsemoyin@gmail.com>
2025-10-12 17:41:32 +0100

    added code to generate graphs comparing japan/south korea stats to oecd average stats. need to globally change column names and then that's it

--- portfolio/data-presentation-project/avg_retirement_age_women.py
+++ portfolio/data-presentation-project/avg_retirement_age_women.py
@@ -6,20 +6,6 @@ import numpy as np
 # the below code formats the 'year' column to be read as a year rather than a number by the analysis modules.
 retirement_age_df = pd.read_csv('retirement_age_men_and_women.csv', parse_dates = ['Year'])
 
-# print(type(retirement_age_df['Year'][0])) # quickly checking that parsing the dates changed the data type properly.
-
-# create a mask for the df and apply it to the main csv.
-
-'''
-oecd_mask = retirement_age_df['Entity'] == 'OECD'
-oecd_df = retirement_age_df[oecd_mask]
-japan_mask = retirement_age_df['Entity'] == 'Japan'
-japan_df = retirement_age_df[japan_mask]
-s_korea_mask = retirement_age_df['Entity'] == 'South Korea'
-s_korea_df = retirement_age_df[s_korea_mask] # using a function would be easier, but i think it'd need
-# new variables for it to work. i'll look into it later. 
-'''
-
 # pd.DataFrame() is used to turn the slices into separate dataframes. if this isn't done, then every operation involving
 # the sliced dfs will come with the same warning. this might also solve the issue of not being able to join
 # the new 'OECD average retirement age' column to the other dfs.
@@ -30,36 +16,31 @@ s_korea_df = pd.DataFrame(retirement_age_df.loc[retirement_age_df['Entity'] == '
 oecd_df['OECD average retirement age'] = (oecd_df['Women_avg_retirement_age']+ oecd_df['Men_avg_retirement_age']) / 2
 
 def column_dropper(dataframe): # quick code to remove the unnecessary columns from the OECD dataframe. functions are fun!
-    droplist = ['Code', 'Women_avg_retirement_age', 'Men_avg_retirement_age']
+    droplist = ['Entity', 'Code', 'Women_avg_retirement_age', 'Men_avg_retirement_age']
     for column in droplist:
         dataframe = dataframe.drop(column, axis=1)
     return dataframe
 
-print(column_dropper(oecd_df))
-# plotting an experimental line graph of japan's retirement rates across the years, including both men and women. 
-# the main goal is to create two graphs that compare japan/south korea's retirement rates to the oecd avg across both genders.
+oecd_df = column_dropper(oecd_df) # set oecd_df's value to the dropped version, in which it only holds the year and 
+# average retirement age columns. 
 
-# japan_df_graph = japan_df.plot(kind = 'line', x = 'Year', 
-#                                y = ['Women_avg_retirement_age', 'Men_avg_retirement_age'], 
-#                                ylabel = ['Average retirement age (women)', 'Average retirement age (men)'], 
-#                                legend = True)
-
-# japan_df_testfig = japan_df_graph.get_figure()
-# japan_df_testfig.savefig("japan_testfig.png") # added some code to test figure creation in vscode.
-# test successful.
-
-# next goal: find OECD average retirement age across both men and women.
-# there are pandas functions to help with this, luckily. 
-
-
+japan_df = pd.merge(japan_df, oecd_df, on='Year') # use pd.merge to join japan_df to oecd_df by the year column.
+s_korea_df = pd.merge(s_korea_df, oecd_df, on='Year')
 
 # making new graphs using the new dataframes.
-'''
-japan_df_graph = japan_df.plot(kind = 'line', x = 'Year', 
-                               y = ['Women_avg_retirement_age', 'Men_avg_retirement_age', 'OECD_avg'], 
-                               ylabel = 'Average retirement age: Japan vs OECD', 
-                               legend = True)
 
-japan_df_figure = japan_df_graph.get_figure()
-japan_df_figure.savefig("japan_vs_oecd_fig.png")
-'''
+japan_vs_oecd_graph = japan_df.plot(kind = 'line', x = 'Year', 
+                               y = ['Women_avg_retirement_age', 'Men_avg_retirement_age', 'OECD average retirement age'], 
+                               ylabel = 'Average retirement age: Japan vs OECD', 
+                               legend = True, title = "Average retirement age: Japan vs OECD average")
+
+japan_vs_oecd_figure = japan_vs_oecd_graph.get_figure()
+japan_vs_oecd_figure.savefig("japan_vs_oecd_fig.png")
+
+s_korea_vs_oecd_graph = s_korea_df.plot(kind = 'line', x = 'Year', 
+                               y = ['Women_avg_retirement_age', 'Men_avg_retirement_age', 'OECD average retirement age'], 
+                               ylabel = 'Average retirement age: South Korea vs OECD', 
+                               legend = True, title = "Average retirement age: South Korea vs OECD average")
+
+s_korea_vs_oecd_figure = s_korea_vs_oecd_graph.get_figure()
+s_korea_vs_oecd_figure.savefig("s_korea_vs_oecd_fig.png")
