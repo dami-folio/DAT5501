00834a2aad61dfa170ef0281cc174a0efb24ebe3
dami-folio <dami.akinsemoyin@gmail.com>
2025-10-16 15:18:59 +0100

    rewrote line generation in a cleaner manner

--- portfolio/line_fitter.py
+++ portfolio/line_fitter.py
@@ -1,4 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random as rd
 
 # random data can be generated using numpy, so i'll define a function to do so. 
+
+gradient = 2.5
+intercept = 2
+
+x_coords = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y_coords = [2]
+
+print(len(x_coords))
+
+for coord in x_coords:
+    if coord == 10:
+        break
+    else:
+        print(coord)
+        y_coordinate = ((gradient * x_coords[coord]) + intercept) + rd.random()
+        y_coords.append(round(y_coordinate, 1))
+        print(y_coordinate, y_coords)
+
+print ("successful!")
