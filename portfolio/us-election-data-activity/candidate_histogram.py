0b2b3d4adebcc6804c0796f2ba09a18651b4765f
dami-folio <dami.akinsemoyin@gmail.com>
2025-10-29 11:34:42 +0000

    added a line of code to collect all unique us states in an array. need to figure out code to count unique counties per state.

--- portfolio/us-election-data-activity/candidate_histogram.py
+++ portfolio/us-election-data-activity/candidate_histogram.py
@@ -4,8 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-us_election_data = pd.read_csv("US-2016-primary.csv", delimiter = ";") # the delimiter removes incorrect separators and replaces them with the typical separator
-unwanted_cols = ["fips"]
+us_election_data = pd.read_csv('US-2016-primary.csv', delimiter = ';') # the delimiter removes incorrect separators and replaces them with the typical separator
+unwanted_cols = ['fips']
 us_election_data = us_election_data.drop(columns = unwanted_cols)
 print(us_election_data.info())
 
@@ -14,3 +14,10 @@ print(us_election_data.info())
 
 sanders_df = pd.DataFrame(us_election_data.loc[us_election_data['candidate'] == 'Bernie Sanders'])
 print(sanders_df.head())
+
+# collect all unique states in a list. then find the average fraction of votes per state.
+# so: avg_fraction_votes = total_fraction_votes_state / num_of_counties
+# collect unique states and number of counties per state.
+
+all_states = np.unique(us_election_data['state'])
+# print(all_states)
