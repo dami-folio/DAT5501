d37d4930f5d242d3bef624a6799414909a321d9d
dami-folio <dami.akinsemoyin@gmail.com>
2025-11-10 12:29:17 +0000

    added code to measure the start and end time of a particular function

--- portfolio/asset-prices-activity/asset_price_calc.py
+++ portfolio/asset-prices-activity/asset_price_calc.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 
 nvda_data = pd.read_csv("NVDA_hist_price_data.csv", parse_dates = ["Date"]) # import the data using pandas, which is more flexible in recongnising datatypes
 
@@ -42,21 +43,26 @@ def closing_price_plot(show_graph):
 # [nvda_data["Close/Last"][0]]
 nvda_daily_price_change = np.array(0.0)
 
-index_point = 1 # aka an iterable index number that moves forward throughout the for-loop to allow for a 
+index_point = 0 # aka an iterable index number that moves forward throughout the for-loop to allow for a 
 # 'rolling difference' of sorts
 
-for item in nvda_data['Close/Last']:
+for item in nvda_data["Close/Last"]:
     price_change_value = nvda_data['Close/Last'][index_point + 1] - nvda_data['Close/Last'][index_point]
     nvda_daily_price_change = np.append(nvda_daily_price_change, price_change_value)
     index_point += 1
-    if index_point == 248:
+    if index_point == len(nvda_data["Close/Last"]) - 1:
         break
 
 # print(nvda_daily_price_change)
 
 # timing the duration of a simple sort function on the price change data...
 
-print(len(nvda_daily_price_change))
+# limit = len(nvda_daily_price_change)
+
+start_time = time.time()
+nvda_daily_price_change[0:8].sort()
+end_time = time.time()
+print(f"{(end_time - start_time) * 1000} ms")
 
 # closing_price_plot(False)
 # print(nvda_data.head())
