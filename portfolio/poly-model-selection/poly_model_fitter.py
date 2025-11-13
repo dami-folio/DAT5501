ce38b0a8e6470e0a032a22b7c04f5830b40dc025
dami-folio <dami.akinsemoyin@gmail.com>
2025-11-13 15:20:14 +0000

    minor changes, changing the color of the graph and changing poly degrees

--- portfolio/poly-model-selection/poly_model_fitter.py
+++ portfolio/poly-model-selection/poly_model_fitter.py
@@ -8,11 +8,11 @@ price_for_light_df_exclude = price_for_light_df.query('1997 > Year > 1905')
 x_val = price_for_light_df_exclude['Year']
 y_val = price_for_light_df_exclude['Price for Light']
 
-polymodel = np.poly1d(np.polyfit(x_val, y_val, 4))
+polymodel = np.poly1d(np.polyfit(x_val, y_val, 11))
 line = np.linspace(1906, 1996, 100)
 # initial_polyfit = np.polyfit(x = price_for_light_df_exclude['Year'], y = price_for_light_df_exclude['Price for Light'], deg = 1)
-plt.scatter(x_val, y_val)
-plt.plot(line, polymodel(line))
+plt.scatter(x_val, y_val, marker = 'x', color = '#fa4b91')
+plt.plot(line, polymodel(line), color = '#7d4bfa')
 plt.show()
 print(price_for_light_df_exclude)
 
