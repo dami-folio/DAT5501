4dd5c72cb321ae5dbee9042216153588a2438fc8
dami-folio <dami.akinsemoyin@gmail.com>
2025-11-13 14:53:54 +0000

    plotted base data as a scatter graph

--- portfolio/poly-model-selection/poly_model_fitter.py
+++ portfolio/poly-model-selection/poly_model_fitter.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 price_for_light_df = pd.read_csv('price-for-light-uk.csv').drop('Code', axis=1).drop('Entity', axis=1)
-price_for_light_df = price_for_light_df.query('Year > 1905')
+price_for_light_df_exclude = price_for_light_df.query('1996 > Year > 1905')
 
-
+initial_polyfit = np.polyfit(x = price_for_light_df_exclude['Year'], y = price_for_light_df_exclude['Price for Light'], deg = 1)
+plt.scatter(x = price_for_light_df_exclude['Year'], y = price_for_light_df_exclude['Price for Light'], marker = '.')
+plt.show()
