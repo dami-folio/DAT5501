57ca3a280cec5ffaa685b65ad32d26cbed91b914
dami-folio <dami.akinsemoyin@gmail.com>
2025-11-20 14:59:25 +0000

    updated polynomial code to add chi squared functionality

--- portfolio/poly-model-selection/poly_model_fitter.py
+++ portfolio/poly-model-selection/poly_model_fitter.py
@@ -25,27 +25,43 @@ include_y_val = price_for_light_df_include['Price for Light']
 poly_order = 3 # global so that they can be used in other functions
 max_poly_order = 15
 
-def polynomial_order_compare():
+plt.scatter(x_val, y_val, marker = '.', color = '#fa4b91', s = 5, label = 'Data points')
+plt.xlabel('Year')
+plt.ylabel('Price for Light (£)')
+plt.title(f'Price for Light (UK): 1906 - 1996\n(Polynomial orders: {poly_order} - {max_poly_order})')
+
+chi_squared_vals = {}
+
+def polynomial_calc():
     global poly_order, max_poly_order
     # line = np.linspace(1906, 1996, 100) # first parameter = starting point, second parameter = ending point, third parameter = sample num to make
-    plt.scatter(x_val, y_val, marker = '.', color = '#fa4b91', s = 5, label = 'Data points')
     # plt.grid()
-    plt.xlabel('Year')
-    plt.ylabel('Price for Light (£)')
-    plt.title(f'Price for Light (UK): 1906 - 1996\n(Polynomial orders: {poly_order} - {max_poly_order})')
     for polynomial in range(poly_order, max_poly_order + 1): # it's max_poly_order + 1 because range does not include the stop value
         # polymodel = np.poly1d(np.polyfit(x_val, y_val, poly_order))
         # plt.plot(line, polymodel(line), lw = 0.6)
-        coefficients = np.polyfit(include_x_val, include_y_val, poly_order)
+        coefficients = np.polyfit(x_val, y_val, poly_order)
         poly = np.poly1d(coefficients)
-        plt.plot(include_x_val, poly(include_x_val), label = f'Poly. order: {poly_order}', lw = 0.6)
+        plt.plot(x_val, poly(x_val), lw = 0.6)
+
+        residuals = poly - y_val # calculating chi2 within the polynomial function
+        chi_squared = 0
+        for residual in residuals:
+            chi_squared += residual ** 2
+        chi_squared_vals.update({f'{poly_order}':'chi_squared'})
+
         poly_order += 1
+
     plt.legend()
     plt.show()
 
+# def polynomial_func(x_coords, y_coords, degree):
+    # coefficients = np.polyfit(x_coords, y_coords, degree)
+    # poly = np.poly1d(coefficients)
+    # return poly
+
 # polynomial_order_compare()
 
-def chi_squared():
+# def chi_squared():
     global poly_order, max_poly_order
     for polynomial in range(poly_order, max_poly_order + 1): # reused from previous function
         coefficients = np.polyfit(x_val, y_val, poly_order)
@@ -53,8 +69,12 @@ def chi_squared():
         # chi_squared_val = np.sum((np.polyval(poly, x_val) - y_val) ** 2)
         chi_squared_val = np.sum(price_for_light_df_include) * (((x_val) - ()) ** 2)
         plt.plot(poly_order, chi_squared_val)
-        print(f'For polynomial of order {poly_order}, chi-squared value is: {chi_squared_val}')
         poly_order += 1
     plt.show()
 
-chi_squared()
+# chi_squared()
+
+# uncertainty = 1
+# observed_val = data value
+# expected_val = polynomial value
+# n = number of data points
