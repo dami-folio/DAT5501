f89db406e8c07c8add266d67c26510ef10a78939
dami-folio <dami.akinsemoyin@gmail.com>
2025-11-24 12:57:25 +0000

    changed code in decision tree code file to a singular import line. it's incomplete right now

--- portfolio/decision-tree-exercise/decision_tree_exercise.py
+++ portfolio/decision-tree-exercise/decision_tree_exercise.py
@@ -1,16 +1,4 @@
-# importing a dataset using code provided on the uc irvine machine learning repository
+# the following code is written using the official scikit-learn documentation
 
-from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-youtube_spam_collection = fetch_ucirepo(id=380) 
-  
-# data (as pandas dataframes) 
-X = youtube_spam_collection.data.features 
-y = youtube_spam_collection.data.targets 
-  
-# metadata 
-print(youtube_spam_collection.metadata) 
-  
-# variable information 
-print(youtube_spam_collection.variables) 
+from sklearn import tree
+
