3402bd04f32b450ed3753a17356e17c58ec1a192
dami-folio <dami.akinsemoyin@gmail.com>
2025-11-27 11:55:52 +0000

    started on decision tree using guidance from datacamp.

--- portfolio/decision-tree-exercise/decision_tree_exercise.py
+++ portfolio/decision-tree-exercise/decision_tree_exercise.py
@@ -1,4 +1,12 @@
-# the following code is written using the official scikit-learn documentation
+# the following code is written using the official scikit-learn documentation and datacamp
 
 from sklearn import tree
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
+yt_spam_df = pd.read_csv("youtube-spam-collection/Youtube01-Psy.csv")
+# print(yt_spam_df.head())
+
+feature_cols = ['CONTENT']
+features = yt_spam_df[feature_cols]
+target = yt_spam_df['CLASS']
