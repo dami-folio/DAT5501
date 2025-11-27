2c9972a23a0460ac0390116353b26d38799242e3
dami-folio <dami.akinsemoyin@gmail.com>
2025-11-27 14:19:45 +0000

    changed dataset and updated code accordingly

--- portfolio/decision-tree-exercise/decision_tree_exercise.py
+++ portfolio/decision-tree-exercise/decision_tree_exercise.py
@@ -6,16 +6,17 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 import pandas as pd
 
-yt_spam_df = pd.read_csv("youtube-spam-collection/Youtube01-Psy.csv")
-# print(yt_spam_df.info())
+wine_qual_df = pd.read_csv("wine-quality/winequality-red.csv", sep = ";")
+# print(wine_qual_df.info())
 
-yt_spam_df = yt_spam_df.convert_dtypes(convert_string=True, convert_integer=True)
-print(yt_spam_df.info())
+wine_qual_df = wine_qual_df.convert_dtypes(convert_string=True, convert_integer=True)
+print(wine_qual_df.info())
 
 # splitting dataset into the feature and target variables. in this case i'll only use the contents as a feature.
-feature_cols = ['CONTENT']
-X = yt_spam_df[feature_cols] # i'm unsure as to why they're marked as X and Y everywhere, but for formatting purposes i'll follow the format
-Y = yt_spam_df['CLASS']
+feature_cols = ['fixed_acidity', 'citric_acid', 'residual_sugar', 'density', 'pH', 'alcohol']
+label_col = ['quality']
+X = wine_qual_df[feature_cols] # i'm unsure as to why they're marked as X and Y everywhere, but for formatting purposes i'll follow the format
+Y = wine_qual_df[label_col]
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3, random_state = 1)
 # the train_test_split() function splits datasets into training and testing sets depending on set sizes provided by the user! super cool
@@ -36,3 +37,5 @@ Y_predict = clf.predict(X_test)
 
 accuracy_rating = metrics.accuracy_score(Y_test, Y_predict)
 print(f"Accuracy: {accuracy_rating}")
+
+tree.plot_tree(clf)
