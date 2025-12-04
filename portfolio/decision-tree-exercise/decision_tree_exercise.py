c7b450c7cf31af91580592f5fc854432534886fb
dami-folio <dami.akinsemoyin@gmail.com>
2025-12-04 10:13:49 +0000

    started recategorising wine quality values

--- portfolio/decision-tree-exercise/decision_tree_exercise.py
+++ portfolio/decision-tree-exercise/decision_tree_exercise.py
@@ -1,11 +1,15 @@
 # the following code is written using the official scikit-learn documentation and datacamp
 
+# PERSONAL NOTE: there has to be a way to clean up all these imports! they're pretty ugly!
+
 from sklearn import tree
+import sklearn.model_selection
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import sklearn
 # for exporting decision tree:
 from sklearn.tree import export_graphviz
@@ -14,12 +18,36 @@ import graphviz
 # from IPython.display import Image
 # import pydotplus
 
+# setting it so that numpy values are printed without the numpy container visible
+
+np.set_printoptions(legacy = "1.13", precision = 4)
 
 wine_qual_df = pd.read_csv("wine-quality/winequality-red.csv", sep = ";")
 # print(wine_qual_df.info())
 
 wine_qual_df = wine_qual_df.convert_dtypes(convert_string=True, convert_integer=True)
-print(wine_qual_df.info())
+# print(wine_qual_df.info())
+
+# categorising the 'quality' column so that wine with quality >= 5 is marked as good quality (1) whereas quality <= 4 is marked as poor quality (0)
+
+poor_quality_val = 0
+good_quality_val = 1
+
+quality_category_dict = {0: poor_quality_val,
+                         1: poor_quality_val,
+                         2: poor_quality_val,
+                         3: poor_quality_val,
+                         4: poor_quality_val,
+                         5: poor_quality_val,
+                         6: good_quality_val,
+                         7: good_quality_val,
+                         8: good_quality_val,
+                         9: good_quality_val,
+                         10: good_quality_val
+                         }
+
+wine_qual_df['quality'].replace(quality_category_dict, inplace = True)
+print(wine_qual_df)
 
 # splitting dataset into the feature and target variables. in this case i'll only use the contents as a feature.
 feature_cols = ['fixed_acidity', 'residual_sugar', 'density', 'pH', 'alcohol']
@@ -51,8 +79,26 @@ print(f"Accuracy: {accuracy_rating}")
 
 fig = plt.figure(figsize = (20, 8))
 sklearn.tree.plot_tree(clf, max_depth = 5, filled = True, feature_names = feature_cols)
-plt.savefig('red_wine_quality.png', dpi = 600)
-# plt.show()
+plt.savefig('red_wine_binary_quality.png', dpi = 600)
+plt.show()
+
+# using KFold to cross-validate the dataset. the code was provided by datacamp
+
+splits = 10
+kf = sklearn.model_selection.KFold(n_splits = splits, shuffle = True, random_state = 55)
+scores = sklearn.model_selection.cross_val_score(clf, X, Y, cv = kf, scoring = "r2")
+
+# using r2 to find the accuracy of each model 
+
+average_r2 = np.mean(scores)
+
+# printing results!!
+
+print(f"R² value for each fold: {scores}")
+print(f"Average R² value: {average_r2}")
+
+# print(kf.split(wine_qual_df))
+
 '''
 
 dot_data = tree.export_graphviz(clf, out_file=None)
